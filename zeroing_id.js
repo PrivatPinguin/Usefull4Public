636159be9b57ac13ef929430132e6edddb7cfca5
AP <60903271+PrivatPinguin@users.noreply.github.com>
2022-07-22 17:03:45 +0200

    Update zeroing_id.js
    recursive add

--- zeroing_id.js
+++ zeroing_id.js
@@ -1,17 +1,17 @@
+var idCounter = 0;
+
 function intToString(x, finalLenght){
 	// fill id with zero chars of given strLenght
 	var xLenght = x.toString().length;
 	if(xLenght > finalLenght){
-		intToString(x, finalLenght+1); // catch error. for example x=100 and finalLenght=1
-    return 0;
+		return intToString(x, finalLenght+1); // catch error. for example x=100 and finalLenght=1
 	}
-	
-
+	//fill zeros with count of new finalLenght
 	x = x.toString();
-	finalLenght -= xLenght //fill zeros with count of new finalLenght
+	finalLenght -= xLenght 
 	while(finalLenght > 0){
 		x = "0" + x;
 		finalLenght -= 1
 	}
-  return(x);
+	return x;
 }
