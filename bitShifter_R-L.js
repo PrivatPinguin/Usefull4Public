9b474070be97a166bad3894a2980d2f726b0c823
AP <60903271+PrivatPinguin@users.noreply.github.com>
2022-08-25 11:28:52 +0200

    Update bitShifter_R-L.js
    now without Math.pow(x,y)

--- bitShifter_R-L.js
+++ bitShifter_R-L.js
@@ -1,6 +1,14 @@
+<html>
+	<head>
+	</head>
+	<body>
+		<script>
+
+
+
 function lShift(num, nShifts=1, bits=8){
-	maxbit= Math.pow(2, bits-1);  //  By the power of Bitscount - Math has the power.
-	bits = (maxbit*2)-1; // is like: Math.pow(2, bits) -1;
+	maxbit = 1 << (bits-1); // is like: Math.pow(2, bits -1);
+	bits = (maxbit << 1)-1; // is like: Math.pow(2, bits) -1;
 	for(let i = 0; i < nShifts; i++){
 		num = num << 1;
 		
@@ -13,7 +21,7 @@ function lShift(num, nShifts=1, bits=8){
 }
 
 function rShift(num, nShifts=1, bits=8){
-	maxbit= Math.pow(2, bits);
+	maxbit = 1 << bits;  // Math.pow(2, bits)
 	for(let i = 0; i < nShifts; i++){
 		if(num%2){
 			num = num + maxbit;
@@ -24,6 +32,10 @@ function rShift(num, nShifts=1, bits=8){
 }
 
 //tst
-console.log( rShift(3) );			// input 0000.0011 -> 1000.0001  || 3 -> 129
+console.log( rShift(3) );		// input 0000.0011 -> 1000.0001  || 3 -> 129
 console.log( rShift(3,1, 4) );		// input 0011 -> 1001
 console.log( lShift(128,1, 8) );	// input 1000.0000 -> 0000.0001  || 128 -> 1
+		
+		</script>
+	</body
+</html>
